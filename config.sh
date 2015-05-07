8774fe918e9b1ceb22f99f562499e994001837ef
hschoonjans <helain90@hotmail.com>
2015-05-07 17:45:30 +0200

    Removed the backslashes. Formatting

--- config.sh
+++ config.sh
@@ -93,69 +93,44 @@ then
 	RABBITMQ_WORKERS="1"
 fi
 
-echo "PREVIEW:\
--------------\
-"
-echo "\
-input {\
-	   log4j {\
-		add_field => $LOG4J_ADD_FIELD # hash (optional), default: {}\
-		codec => $LOG4J_CODEC # codec (optional), default: \"plain\"\
-		data_timeout => $LOG4J_DATA_TIMEOUT # number (optional), default: 5\
-		host => $LOG4J_HOST # string (optional), default: \"0.0.0.0\"\
-		mode => $LOG4J_MODE # string, one of [\"server\", \"client\"] (optional), default: \"server\"\
-		port => $LOG4J_PORT # number (optional), default: 4560\
-	}\
-}\
-output{\
-	rabbitmq {\
-		codec => $RABBITMQ_CODEC # codec (optional), default: \"plain\"\
-		durable => $RABBITMQ_DURABLE # boolean (optional), default: true\
-		exchange => $RABBITMQ_EXCHANGE # string (required)\
-		exchange_type => $RABBITMQ_EXCHANGE_TYPE # string, one of [\"fanout\", \"direct\", \"topic\"] (required)\
-		host => $RABBITMQ_HOST # string (required)\
-		key => $RABBITMQ_KEY # string (optional), default: \"logstash\"\
-		password => $RABBITMQ_PASSWORD # password (optional), default: \"guest\"\
-		persistent => $RABBITMQ_PERSISTENT # boolean (optional), default: true\
-		port => $RABBITMQ_PORT # number (optional), default: 5672\
-		ssl => $RABBITMQ_SSL # boolean (optional), default: false\
-		user => $RABBITMQ_USER # string (optional), default: \"guest\"\
-		verify_ssl => $RABBITMQ_VERIFY_SSL # boolean (optional), default: false\
-		vhost => $RABBITMQ_VHOST # string (optional), default: \"/\"\
-		workers => $RABBITMQ_WORKERS # number (optional), default: 1\
-	}\
-}"
+echo "PREVIEW:
+-------------"
+echo "input {
+	   log4j {
+		add_field => $LOG4J_ADD_FIELD # hash (optional), default: {}
+		codec => $LOG4J_CODEC # codec (optional), default: \"plain\"
+		data_timeout => $LOG4J_DATA_TIMEOUT # number (optional), default: 5
+		host => $LOG4J_HOST # string (optional), default: \"0.0.0.0\"
+		mode => $LOG4J_MODE # string, one of [\"server\", \"client\"] (optional), default: \"server\"
+		port => $LOG4J_PORT # number (optional), default: 4560
+	}
+}
+output{
+	rabbitmq {
+		codec => $RABBITMQ_CODEC # codec (optional), default: \"plain\"
+		durable => $RABBITMQ_DURABLE # boolean (optional), default: true
+		exchange => $RABBITMQ_EXCHANGE # string (required)
+		exchange_type => $RABBITMQ_EXCHANGE_TYPE # string, one of [\"fanout\", \"direct\", \"topic\"] (required)
+		host => $RABBITMQ_HOST # string (required)
+		key => $RABBITMQ_KEY # string (optional), default: \"logstash\"
+		password => $RABBITMQ_PASSWORD # password (optional), default: \"guest\"
+		persistent => $RABBITMQ_PERSISTENT # boolean (optional), default: true
+		port => $RABBITMQ_PORT # number (optional), default: 5672
+		ssl => $RABBITMQ_SSL # boolean (optional), default: false
+		user => $RABBITMQ_USER # string (optional), default: \"guest\"
+		verify_ssl => $RABBITMQ_VERIFY_SSL # boolean (optional), default: false
+		vhost => $RABBITMQ_VHOST # string (optional), default: \"/\"
+		workers => $RABBITMQ_WORKERS # number (optional), default: 1
+	}
+}" > logstash/config.conf
 
-echo "LAUNCHING LOGSTASH:\
------------------------\
-"
+echo "LAUNCHING LOGSTASH:
+-----------------------"
 
-logstash agent -e "\
-input {\
-	   log4j {\
-		add_field => $LOG4J_ADD_FIELD # hash (optional), default: {}\
-		codec => $LOG4J_CODEC # codec (optional), default: \"plain\"\
-		data_timeout => $LOG4J_DATA_TIMEOUT # number (optional), default: 5\
-		host => $LOG4J_HOST # string (optional), default: \"0.0.0.0\"\
-		mode => $LOG4J_MODE # string, one of [\"server\", \"client\"] (optional), default: \"server\"\
-		port => $LOG4J_PORT # number (optional), default: 4560\
-	}\
-}\
-output{\
-	rabbitmq {\
-		codec => $RABBITMQ_CODEC # codec (optional), default: \"plain\"\
-		durable => $RABBITMQ_DURABLE # boolean (optional), default: true\
-		exchange => $RABBITMQ_EXCHANGE # string (required)\
-		exchange_type => $RABBITMQ_EXCHANGE_TYPE # string, one of [\"fanout\", \"direct\", \"topic\"] (required)\
-		host => $RABBITMQ_HOST # string (required)\
-		key => $RABBITMQ_KEY # string (optional), default: \"logstash\"\
-		password => $RABBITMQ_PASSWORD # password (optional), default: \"guest\"\
-		persistent => $RABBITMQ_PERSISTENT # boolean (optional), default: true\
-		port => $RABBITMQ_PORT # number (optional), default: 5672\
-		ssl => $RABBITMQ_SSL # boolean (optional), default: false\
-		user => $RABBITMQ_USER # string (optional), default: \"guest\"\
-		verify_ssl => $RABBITMQ_VERIFY_SSL # boolean (optional), default: false\
-		vhost => $RABBITMQ_VHOST # string (optional), default: \"/\"\
-		workers => $RABBITMQ_WORKERS # number (optional), default: 1\
-	}\
-}"
+if [ -z $DEBUG ];
+then
+    logstash agent -f config.conf
+else
+    logstash agent --configtest -f config.conf
+fi
+
