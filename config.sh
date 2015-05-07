f0b96b95feb7b29bbc9bd825b43234a7082d40e6
hschoonjans <helain90@hotmail.com>
2015-05-07 16:56:37 +0200

    Removed the redundant stdout

--- config.sh
+++ config.sh
@@ -93,7 +93,7 @@ then
 	RABBITMQ_WORKERS="1"
 fi
 
-logstash agent -e "\
+echo "\
 input {\
 	   log4j {\
 		add_field => $LOG4J_ADD_FIELD # hash (optional), default: {}\
@@ -122,5 +122,34 @@ output{\
 			vhost => $RABBITMQ_VHOST # string (optional), default: \"/\"\
 			workers => $RABBITMQ_WORKERS # number (optional), default: 1\
 		}\
+	}\
+}"
+logstash agent -e "\
+input {\
+	   log4j {\
+		add_field => $LOG4J_ADD_FIELD # hash (optional), default: {}\
+		codec => $LOG4J_CODEC # codec (optional), default: \"plain\"\
+		data_timeout => $LOG4J_DATA_TIMEOUT # number (optional), default: 5\
+		host => $LOG4J_HOST # string (optional), default: \"0.0.0.0\"\
+		mode => $LOG4J_MODE # string, one of [\"server\", \"client\"] (optional), default: \"server\"\
+		port => $LOG4J_PORT # number (optional), default: 4560\
+	}\
+}\
+output{\
+	rabbitmq {\
+		codec => $RABBITMQ_CODEC # codec (optional), default: \"plain\"\
+		durable => $RABBITMQ_DURABLE # boolean (optional), default: true\
+		exchange => $RABBITMQ_EXCHANGE # string (required)\
+		exchange_type => $RABBITMQ_EXCHANGE_TYPE # string, one of [\"fanout\", \"direct\", \"topic\"] (required)\
+		host => $RABBITMQ_HOST # string (required)\
+		key => $RABBITMQ_KEY # string (optional), default: \"logstash\"\
+		password => $RABBITMQ_PASSWORD # password (optional), default: \"guest\"\
+		persistent => $RABBITMQ_PERSISTENT # boolean (optional), default: true\
+		port => $RABBITMQ_PORT # number (optional), default: 5672\
+		ssl => $RABBITMQ_SSL # boolean (optional), default: false\
+		user => $RABBITMQ_USER # string (optional), default: \"guest\"\
+		verify_ssl => $RABBITMQ_VERIFY_SSL # boolean (optional), default: false\
+		vhost => $RABBITMQ_VHOST # string (optional), default: \"/\"\
+		workers => $RABBITMQ_WORKERS # number (optional), default: 1\
 	}\
 }"
